447d489c2d106d41f06f589cbb0ee4fd3c08d569
Om Prakash Pandey <pandeyomu16.gol@gmail.com>
2020-11-01 18:43:59 +0530

    add push order purchase list middleware

--- controllers/user.js
+++ controllers/user.js
@@ -58,3 +58,34 @@ exports.getUserPurchaseList = (req, res) => {
       return res.json(order);
     });
 };
+
+exports.pushOrderInPurchaseList = (req, res, next) => {
+  let purchases = [];
+  req.body.order.products.forEach((product) => {
+    purchases.push({
+      _id: product._id,
+      name: product.name,
+      description: product.description,
+      category: product.category,
+      quantity: product.quantity,
+      amount: req.body.order.amount,
+      transaction_id: req.body.order.transaction_id,
+    });
+  });
+
+  // store into DB
+  User.findOneAndUpdate(
+    { _id: req.profile._id },
+    { $push: { purchases: purchases } },
+    { new: true },
+    (err, purchases) => {
+      if (err || !purchases) {
+        return res.status(400).json({
+          msg: "Unable to update purchase list",
+        });
+      }
+    }
+  );
+
+  next();
+};
