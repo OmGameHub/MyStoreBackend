6d6f59a7b8fb41b31de5efc8b6752cb0487b526e
Om Prakash Pandey <pandeyomu16.gol@gmail.com>
2020-11-02 20:29:36 +0530

    add comments

--- routes/user.js
+++ routes/user.js
@@ -10,10 +10,22 @@ const { isSignedIn, isAuthenticated, isAdmin } = require("../controllers/auth");
 
 router.param("userId", getUserById);
 
+// @type    GET
+// @route   /api/user/:userId
+// @desc    route for get user information
+// @access  PRIVATE
 router.get("/user/:userId", isSignedIn, isAuthenticated, getUser);
 
+// @type    PUT
+// @route   /api/user/:userId
+// @desc    route for update user information
+// @access  PRIVATE
 router.put("/user/:userId", isSignedIn, isAuthenticated, updateUser);
 
+// @type    GET
+// @route   /api/orders/user/:userId
+// @desc    route for get user purchase list
+// @access  PRIVATE
 router.get(
   "/orders/user/:userId",
   isSignedIn,
